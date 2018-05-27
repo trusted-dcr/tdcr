a185948f1dca02b1ae055a50d9e6b54207128607
Malthe Kirkbro <maek@itu.dk>
2018-05-27 23:14:57 +0200

    Fix sgxd.proto C# namespacing

--- TDCR.CoreLib/Messages/Config/SgxConfig.cs
+++ TDCR.CoreLib/Messages/Config/SgxConfig.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
-using Tdcr.Sgxd;
 using TDCR.CoreLib.Messages.Network;
 using TDCR.CoreLib.Wire.Dcr;
 
 namespace TDCR.CoreLib.Messages.Config
 {
-    public class SgxConfig : IPayload<Tdcr.Sgxd.SgxConfig>
+    public class SgxConfig : IPayload<Wire.Sgx.SgxConfig>
     {
         public Uid UID { get; set; }
         public Workflow Workflow { get; set; }
         public Peer[] Peers { get; set; }
 
-        public Tdcr.Sgxd.SgxConfig ToWire()
+        public Wire.Sgx.SgxConfig ToWire()
         {
-            var conf = new Tdcr.Sgxd.SgxConfig
+            var conf = new Wire.Sgx.SgxConfig
             {
                 Self = UID.ToWire(),
                 Workflow = Workflow
             };
 
             foreach (var p in Peers)
-                conf.Peers.Add(new Tdcr.Sgxd.SgxConfig.Types.Peer { Address = BitConverter.ToUInt32(p.Address.GetAddressBytes().Reverse().ToArray(), 0) });
+                conf.Peers.Add(new Wire.Sgx.SgxConfig.Types.Peer { Address = BitConverter.ToUInt32(p.Address.GetAddressBytes().Reverse().ToArray(), 0) });
 
             return conf;
         }
